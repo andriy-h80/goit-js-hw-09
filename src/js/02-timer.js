b148b8fca53a097e161aeb28679126305d9e3e29
andriy-h80 <andrey.hanzel@gmail.com>
2023-03-16 17:04:19 +0200

    task-2 done (almost)

---
+++ src/js/02-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+// all modules
+import Notiflix from 'notiflix';
+
+let selectedDate = null;
+
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    input: document.querySelector('#datetime-picker'),
+    dataDays: document.querySelector('[data-days]'),
+    dataHours: document.querySelector('[data-hours]'),
+    dataMinutes: document.querySelector('[data-minutes]'),
+    dataSeconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+refs.startBtn.addEventListener('click', onStartTimerClick);
+// мінімальне оформлення елементів інтерфейсу
+refs.startBtn.style.backgroundColor = 'lightblue';
+document.body.style.backgroundColor = 'lightyellow';
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      if(selectedDates[0] < Date.now()) {
+        Notiflix.Report.warning('Please choose a date in the future');
+      } else {
+        refs.startBtn.disabled = false;
+        selectedDate = selectedDates[0];
+      }
+    },
+// згідно документації flatpickr дуже рекомендується вмикати цей параметр
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+//  можливість вводити дату безпосередньо в поле введення
+    allowInput: true,
+  };
+
+flatpickr(refs.input, options);
+
+function onStartTimerClick() {
+    setInterval(() => {
+        const currentDate = Date.now();
+        const deltaTime = selectedDate - currentDate;
+        renderTimer(convertMs(deltaTime));
+        refs.startBtn.disabled = true;
+        // refs.input.disabled = true;
+    }, 1000);
+};
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+};
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  
+    return { days, hours, minutes, seconds };
+};
+
+function renderTimer({ days, hours, minutes, seconds }) {
+  refs.dataDays.textContent = days;
+  refs.dataHours.textContent = hours;
+  refs.dataMinutes.textContent = minutes;
+  refs.dataSeconds.textContent = seconds;
+};
