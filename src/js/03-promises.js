7d0558696acc0cfe6c894354de7599fc16c0b612
andriy-h80 <andrey.hanzel@gmail.com>
2023-03-18 13:58:36 +0200

    all tasks done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,44 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  startCreatePromisesBtn: document.querySelector('.form'),
+  firstDelay: document.querySelector('[name="delay"]'),
+  delayStep: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
+refs.startCreatePromisesBtn.addEventListener('submit', onCreatePromisesBtnStart);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
+};
 
-  
+function onCreatePromisesBtnStart(event) {
+  event.preventDefault();
+
+  let firstDelayValue = Number(refs.firstDelay.value);
+  let delayStepValue = Number(refs.delayStep.value);
+  let amountValue = Number(refs.amount.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    let promiseDelay = firstDelayValue + delayStepValue * i;
+
+    createPromise(i, promiseDelay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  } 
+};
